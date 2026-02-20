0cb9561e047cef55caac32cc9234490d5fecfc00
kaushiksai29 <51222741+kaushiksai29@users.noreply.github.com>
2026-02-20 16:50:22 -0500

    Add rewrites for media image handling

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/media/:section/:uuid([\\w-]+)_rw_:size(\\d+).:ext(jpg|jpeg|png|JPG|JPEG|PNG)",
+        destination: "/media/:section/:uuid_rw_600.:ext",
+      },
+      {
+        source: "/media/:section/:uuid([\\w-]+)_rw_:size(\\d+).JPG",
+        destination: "/media/:section/:uuid_rw_600.jpg",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
